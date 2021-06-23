86d5710a2c95200ea23d6f7f8acabf3de1f156ce
isabellagh <isabellagh88@gmail.com>
2021-06-23 14:57:05 -0400

    TextInput boilerplate

---
+++ src/components/TextInput.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TextInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <label>Sample Label</label>
+        <input type="text" placeholder="Placeholder" />
+      </div>
+    );
+  }
+}
+
+export default TextInput;
