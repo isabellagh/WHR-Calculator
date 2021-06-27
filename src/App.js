cdc10135580783236dbe26f1ae81f38bc9d15c31
isabellagh <isabellagh88@gmail.com>
2021-06-27 14:50:29 -0400

    hardcoded trainers info link to

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-// import Navbar from './components/layout/Navbar'
-// import Form from './components/Form';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Welcome from "./pages/Welcome";
-import Trainers from "./pages/Trainers"
-import TrainerInfo from "./pages/TrainerInfo"
+import Trainers from "./pages/Trainers";
+import TrainerInfo from "./pages/TrainerInfo";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import Clients from "./pages/Clients";
@@ -17,24 +15,26 @@ class App extends Component {
       <div className="App">
         <Navbar title="WHR Calculator" icon="fas fa-heartbeat" />
         <main>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/trainers">
-            <Trainers />
-          </Route>
-          <Route path="/trainer-info/:id">
-            <TrainerInfo />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/clients">
-            <Clients />
-          </Route>
+          <Switch>
+            <Route path="/welcome">
+              <Welcome />
+            </Route>
+            <Route path="/trainers" exact>
+              <Trainers />
+            </Route>
+            <Route path="/trainers/:id">
+              <TrainerInfo />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/clients">
+              <Clients />
+            </Route>
+          </Switch>
         </main>
       </div>
     );
--- src/pages/Trainers.js
+++ src/pages/Trainers.js
@@ -1,15 +1,19 @@
+import { Link } from 'react-router-dom'
 
 const Trainers = () => {
     return (
       <section>
         <h1>List of our Trainers</h1>
         <ul>
-          <li>Trainer 1</li>
-          <li>Trainer 2</li>
-          <li>Trainer 3</li>
-          <li>Trainer 4</li>
-          <li>Trainer 5</li>
-          <li>Trainer 6</li>  
+          <li>
+            <Link to='/trainers/1'>Trainer 1</Link>
+          </li>
+          <li>
+            <Link to='/trainers/2'>Trainer 2</Link>
+          </li>
+          <li>
+            <Link to='/trainers/3'>Trainer 3</Link>
+          </li>
         </ul>
       </section>
     )
