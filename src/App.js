e334c648b1a4912e1ec74795d04f2d896d67c697
isabellagh <isabellagh88@gmail.com>
2021-06-28 14:01:36 -0400

    componentDidMount users fetch working

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ import Clients from "./pages/Clients";
 import "./App.css";
 
 class App extends Component {
+  
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/users')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
+  
   render() {
     return (
       <div className="App">
