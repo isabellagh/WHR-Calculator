813d266d9bf802fb7c5e269e70379ce63d856025
isabellagh <isabellagh88@gmail.com>
2021-07-02 00:05:50 -0400

    registration set up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,10 @@
+import Registration from "../components/auth/Registration"
 
 const Signup = () => {
   return (
     <div>
       <h1>Sign up as a Trainer</h1>
+      <Registration />
     </div>
   )
 }
