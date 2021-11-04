d482007a221c210378fcb3eb535fbd0ad5384dea
isabellagh <isabellagh88@gmail.com>
2021-11-04 15:36:06 -0400

    trainers and clients change background

--- src/components/trainers/TrainerInfo.js
+++ src/components/trainers/TrainerInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+// import './trainers-info.css'
 // import { Link } from 'react-router-dom'
 // import TrainersList from './TrainersList';
 
@@ -13,19 +14,26 @@ const TrainerInfo = (props) => {
   //attrubutes vanish after refreshing the page, fixed
 
   return (
-    <div className="container">
-      <br /> <br />
-      <div class="row g-0 d-inline-block">
-        <img className="" src={user ? user.image : null} style={{ width: '400px'}} alt="" />
-        <div className="col-md-8 d-inline-block">
+    <div className="trainers-container">
+      <div
+        class="trainers-wrapper"
+        style={{  }}
+      >
+        <img
+          className="trainers-img"
+          src={user ? user.image : null}
+          style={{ width: "100%", height: "100%" }}
+          alt=""
+        />
+        <div className="trainers-card">
           <div className="card-body">
             <h2 className="card-title">{user ? user.name : null}</h2>
-            <p class="card-text">
+            <p class="trainers-p">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
               longer.
             </p>
-            <h5 className="card-text">
+            <h5 className="trainers-h2">
               Contact me here: {user ? user.email : null}
             </h5>
           </div>
