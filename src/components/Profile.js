a36a1cf0d176a89926d3f54d40bb0ecf54dc1dcd
isabellagh <isabellagh88@gmail.com>
2021-11-04 22:42:10 -0400

    clients directory added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
 //! null attributes when page refreshed
 
 const Profile = (props) => (
-    <div className="container">
+  <div className="container">
     {/* {console.log("profile", props)} */}
-        <br />
-        <h1 className="text-center">{props.currentUser && props.currentUser.attributes.name}</h1>
-        <img src={props.currentUser.attributes.image} className="img-thumbnail" style={{ width: '400px'}} alt="..." />
-        <h4 className="text-center">{props.currentUser && props.currentUser.attributes.email}</h4>
-    </div>
-)
-const mapStateToProps = state => {
-  return {currentUser: state.currentUser}
-}
+    <br />
+    <h1 className="text-center">
+      {props.currentUser && props.currentUser.attributes.name}
+    </h1>
+    <img
+      src={props.currentUser.attributes.image}
+      className="img-thumbnail"
+      style={{ width: "400px" }}
+      alt="..."
+    />
+    <h4 className="text-center">
+      {props.currentUser && props.currentUser.attributes.email}
+    </h4>
+  </div>
+);
+const mapStateToProps = (state) => {
+  return { currentUser: state.currentUser };
+};
 
-export default connect(mapStateToProps)(Profile)
-
-
+export default connect(mapStateToProps)(Profile);
